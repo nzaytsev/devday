2635d83ec18cd9742e5ba9f5a524e4e3038376d3
nzaytsev <nikolai.zaytsev@gitkraken.com>
2024-09-30 15:10:32 +0700

    add env vars

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello() {
-    return { body: 'app-server' };
+    return { body: 'app-server', env: '' + process.env.environment };
   }
   checkStatus() {
     return null;
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
 
+dotenv.config({ path: `../.env.${process.env.NODE_ENV}` });
+console.log('environment=' + process.env.NODE_ENV);
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
