32d9bdc4cb0cb5912a217f11e86f14ffd64347a0
nzaytsev <nikolai.zaytsev@gitkraken.com>
2024-10-01 13:48:25 +0700

    add more fields

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,7 +5,8 @@ export class AppService {
   getHello() {
     return {
       body: 'app-server',
-      autodeploy: 'yep',
+      autodeploy: 'yep.2',
+      morefield: 123456,
     };
   }
   checkStatus() {
