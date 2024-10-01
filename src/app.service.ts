ca978b116bd848adf54d92add27a75aa3488dde7
nzaytsev <nikolai.zaytsev@gitkraken.com>
2024-10-01 12:49:10 +0700

    test auro deploy

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,11 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello() {
-    return { body: 'app-server', env: '' + process.env.environment };
+    return {
+      body: 'app-server',
+      env: '' + process.env.environment,
+      autodeploy: 'yep',
+    };
   }
   checkStatus() {
     return null;
